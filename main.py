b06040f4e3e7ad8f74bc4bcbbe86530fda71da9d
Prakhar Katiyar <katiyar.4@iitj.ac.in>
2022-08-10 00:09:24 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#This python code converts any Amazon India link to an affiliate link and also shortens it.
+#Library used: pyshorteners
+
+
+import pyshorteners
+
+#Taking the URL as an input
+url = input('Enter the url: ')
+#Adding my Amazon Associate ID to the amazon.in URL
+url = url + '?tag=prakharkati00-21'
+
+
+#Shortening the amazon.in URL, with the tag ingluded
+def shortenurl(url):
+    s = pyshorteners.Shortener()
+    print(s.tinyurl.short(url))
+
+shortenurl(url)
